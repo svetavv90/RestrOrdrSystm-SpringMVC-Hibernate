3a85fb82c971f01c364591605cbfd12c045c1cb1
svetavv90 <svetavv90@i.ua>
2015-07-31 17:58:39 +0200

    Update OrderDAO.java

--- src/main/java/test/sprng/OrderDAO.java
+++ src/main/java/test/sprng/OrderDAO.java
@@ -7,9 +7,11 @@ import java.util.Map;
  * Created by Svitlana on 29-May-15.
  */
 public interface OrderDAO {
-    public List<Menu> selectAllMenu();
     public List<Menu> selectMeal(Meal m, Cuisines c);
     public Meal choiceMeal(Integer n);
     public Cuisines choiceCuisines(Integer c);
-    public List<Menu> menu(String select);
+    public List<SelectedMenu> menu(int id);
+    public List<SelectedMenu> allSelected();
+    public void deleteAllRecord();
+    public void addToTrans();
 }
